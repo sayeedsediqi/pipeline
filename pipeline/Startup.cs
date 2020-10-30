c7e3dbc1f4e9a735c86c99d7c00407fe4cad7134
Sayeed <60190007+sayeedsediqi@users.noreply.github.com>
2020-10-30 18:42:25 -0400

    Update Startup.cs

--- pipeline/Startup.cs
+++ pipeline/Startup.cs
@@ -23,6 +23,8 @@ namespace pipeline
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            
+         //this is my DevOps test pipeline
             services.AddRazorPages();
         }
 
